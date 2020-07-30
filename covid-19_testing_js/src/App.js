0757d37663574f75df5e4f6c0005a6ffe640f00f
juan <1juanorta@gmail.com>
2020-07-30 16:30:41 -0500

    Created Node/React work work environment

---
+++ covid-19_testing_js/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+	state = {};
+
+	render() {
+		return (
+			<div>
+				<h1>COVID-19 Test Locations</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
