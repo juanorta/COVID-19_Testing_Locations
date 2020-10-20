5d3728288540e874d26ff5f51694bf83d82f7cff
juan <1juanorta@gmail.com>
2020-10-20 16:46:05 -0500

    reverted back

--- covid-19_testing_js/src/client/components/Home/Home.js
+++ covid-19_testing_js/src/client/components/Home/Home.js
@@ -10,7 +10,9 @@ class Home extends Component {
 	render() {
 		return (
 			<div className="home">
-				<h1 className="line anim-typewriter">sup negas</h1>
+				<h1 className="line anim-typewriter">
+					COVID-19 Testing Location Finder
+				</h1>
 
 				<CityForm />
 			</div>
